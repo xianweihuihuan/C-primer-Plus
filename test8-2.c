dbdeb03ebd26ccee9876ebc7e49071fd203fff4f
xianweihuihuan <2907404984@qq.com>
2024-10-31 01:17:24 +0800

    第八章第二题

---
+++ test8-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char a[100]={0};
+	scanf("%s",a);
+	int i = 0;
+	int b = 0;
+	int c = 0;
+	int sz = sizeof(a);
+	for(i;i<sz;i++){
+		if(a[i]>='a'&&a[i]<='z'){
+			b++;
+		}
+		else if(a[i]>='A'&&a[i]<='Z'){
+			c++;
+		}
+	}
+	printf("大写字母:%d,小写字母:%d",c,b);
+	return 0;
+}
